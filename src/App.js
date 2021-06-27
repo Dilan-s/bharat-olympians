fdb8bbf02eecc8bcbddaba6fa305e9072578890f
Dilan Sheth <ds1619@ic.ac.uk>
2021-06-27 18:03:28 +0100

    [Dilan Sheth] Introduced routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
 import HomePage from './pages/home/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+        <Route exact path='/' component={HomePage} />
+      </BrowserRouter>
     </div>
   );
 }
